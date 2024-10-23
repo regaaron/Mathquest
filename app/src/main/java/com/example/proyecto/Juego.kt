b047cc0ad55d6278fd183d43dd539fed8e5ddd51
regaaron <aaron_lopez222@hotmail.com>
2024-10-23 12:12:48 -0600

    Primer pantalla funcionando

---
+++ app/src/main/java/com/example/proyecto/Juego.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Juego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jugar)
+    }
+
+}
--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +13,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var btnJugar = findViewById<ImageButton>(R.id.btnJugar)
+        btnJugar.setOnClickListener {
+            val intent = Intent(this, Juego::class.java)
+            startActivity(intent)
+        }
+
+        var btnAyuda = findViewById<ImageButton>(R.id.btnAyuda)
+        btnAyuda.setOnClickListener {
+            val intent = Intent(this, Ayuda::class.java)
+            startActivity(intent)
+        }
+
+        var btnSalir = findViewById<ImageButton>(R.id.btnSalir)
+        btnSalir.setOnClickListener{
+            Toast.makeText(this, "Gracias por jugar", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
